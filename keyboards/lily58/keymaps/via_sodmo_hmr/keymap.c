9dbc4d77ea6dae97fa771a121260c84a16878bc3
Yarden Sod-Moriah <yardnsm@gmail.com>
2025-06-21 17:01:53 +0300

    Bug fixes

--- keyboards/lily58/keymaps/via_sodmo_hmr/keymap.c
+++ keyboards/lily58/keymaps/via_sodmo_hmr/keymap.c
@@ -61,9 +61,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  [_QWERTY] = LAYOUT(
   XXXXXXX,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                     KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    XXXXXXX,
   XXXXXXX,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    XXXXXXX,
-  KC_LSFT,    HM_A,   HM_S,    HM_D,    HM_F,    KC_G,                     KC_H,    HM_J,    HM_K,    HM_L,    HM_SCLN, KC_RSFT,
-  KC_ESC,   KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, KC_LBRC,  KC_RBRC,  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_QUOT,
-                     XXXXXXX, KC_LGUI, LOWER,   KC_SPC,                   KC_ENT,  RAISE,   KC_BCPS, XXXXXXX
+  KC_LSFT,   HM_A,   HM_S,    HM_D,    HM_F,    KC_G,                     KC_H,    HM_J,    HM_K,    HM_L,    HM_SCLN, KC_RSFT,
+  KC_ESC,    KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, KC_LBRC,  KC_RBRC,  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_QUOT,
+                     XXXXXXX, KC_LGUI, LOWER,   KC_SPC,                   KC_ENT,  RAISE,   KC_BSPC, XXXXXXX
 ),
 /* LOWER
  * ,-----------------------------------------.                    ,-----------------------------------------.
